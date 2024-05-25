9034897c6a133927088d9729ed91f304f13bc5e5
Fahim Fahrey <mehmetfaahem@gmail.com>
2024-05-25 20:38:55 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const handleOpenDrawer = () => {
+  document.getElementById("drawer").style.display = "flex";
+};
+
+const handleCloseDrawer = () => {
+  document.getElementById("drawer").style.display = "none";
+};
